9226c821624cbfa3531a41688894bc5c404b764c
Darek Finster <dariusz.finster@gmail.com>
2022-10-19 19:55:14 +0200

    update

--- app/javascript/controllers/random_controller.js
+++ app/javascript/controllers/random_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import confetti from "canvas-confetti"
 
 export default class extends Controller {
-  static targets = ['answers', 'answer']
+  static targets = ['answers', 'answer', 'next', 'failure']
+
+  connect() {
+    this.nextTarget.classList.add('d-none')
+  }
 
   check(event) {
     event.preventDefault()
@@ -16,12 +21,22 @@ export default class extends Controller {
           })
           event.target.classList.remove('btn-danger')
           event.target.classList.add('btn-success')
+          this.nextTarget.classList.remove('d-none')
+          this.failureTarget.classList.add('d-none')
+          confetti({
+            particleCount: 70,
+            angle: 120,
+            spread: 65,
+            origin: { x: 1 }
+          })
         } else {
           this.answerTargets.forEach(element => { 
             if (!element.classList.contains('btn-success')) {
               element.classList.add('btn-danger')
             }
           })
+          this.nextTarget.classList.add('d-none')
+          this.failureTarget.classList.remove('d-none')
         }
       })
       .catch(console.error);
