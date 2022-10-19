8c89f4365fd2ed06c21073c920863507caac5751
Darek Finster <dariusz.finster@gmail.com>
2022-10-19 16:01:54 +0200

    update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "ISMF HQ <darek.finster@kw.krakow.pl>"
+  config.mailer_sender = "ISMF HQ <no-reply@ismf-ski.org>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
