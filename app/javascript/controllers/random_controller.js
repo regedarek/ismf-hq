db9c48f811acb31da3e6b9785f43813b8c4e52b9
Darek Finster <dariusz.finster@gmail.com>
2022-11-15 19:45:02 +0100

    update

--- app/javascript/controllers/random_controller.js
+++ app/javascript/controllers/random_controller.js
@@ -7,6 +7,12 @@ export default class extends Controller {
   connect() {
     this.nextTarget.classList.add('d-none')
     this.remarksTarget.classList.add('d-none')
+
+    this.element.querySelectorAll('a').forEach(function(link) {
+      if (link.host !== window.location.host) {
+        link.target = "_blank"
+      }
+    })
   }
 
   check(event) {
